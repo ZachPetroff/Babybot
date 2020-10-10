ceb1bc239d1eecd204fd64e44c51ce6d5df2de8d
ZachPetroff <59905848+ZachPetroff@users.noreply.github.com>
2020-10-09 20:11:57 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 def plot_movements(seperations, connection_changes, mins, ram_mean, lam_mean, rlm_mean, llm_mean):
     for seperation in range(len(seperations)):
         if seperation != 0:
