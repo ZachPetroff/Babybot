8dbb8686aeefd8d193c2e477401319ddfc453985
ZachPetroff <59905848+ZachPetroff@users.noreply.github.com>
2020-10-24 14:31:33 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 class Mobile:
   def __init__(self, average_window=.5):
@@ -27,10 +26,7 @@ class Babybot:
       non_contigent=False, nc_rate=.8, mobile_on=True, mobile_window=0.025):
     self.baseline_rates = baseline_rates
     self.rates = baseline_rates # rates per minute of each limb moving
-    if mobile_on:
-        self.reward = [reward, reward_flux]  # reward is the center of the distribution and reward_flux is the scale or width of the distribution
-    else:
-        self.reward = [reward, mobile_window]
+    self.reward = [reward, reward_flux]  # reward is the center of the distribution and reward_flux is the scale or width of the distribution
     self.cost = cost # removal of rate per minute for each non-rewarded movement
     self.expectation = [0, 0, 0, 0] # subtracted from both reward and punishment
     self.expectation_growth = expectation_growth # growth of expectation for each rewarded movement
@@ -55,6 +51,10 @@ class Babybot:
           and not self.connected and (self.expectation > 0).any()): # reward state
       for limb in range(len(self.limbs)):
         # creates the peaking effect shown in data, the movements peak around 35 and then decrease to around 30 (fatigue, boredom)
+        
+        if self.mobile_on and self.reward_flux != 0:
+            self.reward[0] = self.mobile.reward_window
+        
         self.rates[limb] += (np.random.normal(self.reward[0], self.reward[1]) - self.expectation[limb]) * moves[limb]  # increases rates if reward > expectation, decreases otherwise
         
         self.expectation[limb] += self.expectation_growth * moves[limb]  
@@ -152,49 +152,4 @@ class Babybot:
      
     return r_arm_moves, l_arm_moves, r_leg_moves, l_leg_moves, r_arm_expectations, l_arm_expectations, r_leg_expectations, l_leg_expectations, mins, connected_moves, self.mobile.moves, nr, mr
 
-def plot_movements(seperations, connection_changes, mins, ram_mean, lam_mean, rlm_mean, llm_mean):
-    for seperation in range(len(seperations)):
-        if seperation != 0:
-            plt.plot(mins[seperations[seperation-1]:seperations[seperation]], ram_mean[seperations[seperation-1]:seperations[seperation]], 'red', label="Connected Limb Movements")
-            plt.plot(mins[seperations[seperation-1]:seperations[seperation]], lam_mean[seperations[seperation-1]:seperations[seperation]], 'black', label="Disconnected Limb Movements")
-            plt.plot(mins[seperations[seperation-1]:seperations[seperation]], rlm_mean[seperations[seperation-1]:seperations[seperation]], 'black')
-            plt.plot(mins[seperations[seperation-1]:seperations[seperation]], llm_mean[seperations[seperation-1]:seperations[seperation]], 'black')
-            plt.legend()
-
-            for loc in list(connection_changes)+[len(mins)]:
-                plt.axvline(loc, color='black', linestyle='dashed')
-            plt.xlim(seperations[seperation-1], seperations[seperation])
-            plt.ylim(0, None)
-            plt.ylabel("Movements")
-            plt.xlabel("Minutes")
-            plt.title("Session {}".format(seperation) + "\n" + "Disconnected" + " "*15 + "Connected" + " "*15 + "Disconnected")
-            plt.show()
-            
-def plot_minute(minute, cms, mms, timestep):
-    steps_per_min = 1 / timestep
-    
-    cms_slice = cms[int(minute * steps_per_min):int((minute * steps_per_min) + steps_per_min)]
-    mms_slice = mms[int(minute * steps_per_min):int((minute * steps_per_min) + steps_per_min)]
-    
-    steps_per_min = np.arange(0, steps_per_min)
-    
-    plt.plot(steps_per_min, cms_slice, "red", label="Connected Limb Movements")
-    plt.plot(steps_per_min, mms_slice, "black", label="Mobile Movements")
-    plt.legend(bbox_to_anchor=(1.6, 1))
-    plt.title("Minute {}".format(str(minute)))
-    plt.show()
-    
-def plot_expectation(mins, reward, rae_mean, lae_mean, rle_mean, lle_mean):
-    plt.plot(mins, rae_mean, 'red', label="Connected Limbs")
-    plt.plot(mins, lae_mean, "black", label="Disconnected Limbs")
-    plt.plot(mins, rle_mean, "black")
-    plt.plot(mins, lle_mean, "black")
-    plt.xlabel("Minutes")
-    plt.ylabel("Expectation")
-    plt.title("Expectation Over Time")
-    plt.plot([0, 60], [reward, reward], "yellow", label="Reward")
-
-    plt.legend()
-    plt.show()
-    
 
