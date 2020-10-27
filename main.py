87a0c23e4c3bd204f8b03ad1c0c942939e49cab5
ZachPetroff <59905848+ZachPetroff@users.noreply.github.com>
2020-10-27 13:10:59 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -141,8 +141,7 @@ class Babybot:
                   
       for step in range(steps_per_min):
         moves = self.move()
-        if step % self.learning_lag == 0:
-            self.update_rates(moves)
+        self.update_rates(moves)
         connected_moves.append(moves[self.connected_limb])
         
         if self.mobile.moving:           # updates mobile moves: 1 = moving, 0 = not moving
